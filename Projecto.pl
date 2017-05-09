71406dd55edd385f7260b4706b0e36ed2c9716fd
Bernardo Esteves <esteveste@users.noreply.github.com>
2017-05-09 16:28:16 +0100

    Propaga v1

--- Projecto.pl
+++ Projecto.pl
@@ -24,3 +24,45 @@ tira_num_aux(Num,Puz,(L,C),N_Puz):-
 
 tira_num(Num,Puz,Posicoes,N_Puz):- percorre_muda_Puz(Puz,tira_num_aux(Num),Posicoes,N_Puz).
 
+
+%puzzle_muda_propaga(Puz, Pos, Cont, N_Puz) faz o mesmo que o predicado
+%puzzle_muda/4, mas, no caso de Cont ser uma lista unitária, propaga a mudança, isto
+%é, retira o número em Cont de todas as posições relacionadas com Pos, isto é, todas as
+%posições na mesma linha, coluna ou bloco.
+
+
+
+puzzle_muda_propaga(Puz, (L,C), [Cont], N_Puz):-
+        e_lista_unitario([Cont]),!,%verifica se Cont e unitario nao verifica mais hipoteses
+        puzzle_muda(Puz, (L,C), [Cont], Puz_Muda),%altera o local do puz por Cont
+        posicoes_relacionadas((L,C),PosRel),%arranja as posicoes relacionadas a Pos
+        tira_num(Cont,Puz_Muda,PosRel,Puz_Tira),
+        puzzle_muda_propaga_aux(PosRel,Puz_Tira,N_Puz).
+        
+
+
+puzzle_muda_propaga(Puz,(L,C),Cont,N_Puz):-
+        \+ e_lista_unitario(Cont),%se o resto dos argumentos nao for vazio, ent n e unitario
+        puzzle_muda(Puz,(L,C),Cont,N_Puz).
+
+%auxiliar para ver onde propagar
+puzzle_muda_propaga_aux([],Puz,Puz):-!.%se chegou ao fim ent o puz final e o do tira
+
+puzzle_muda_propaga_aux([H_PosRel|_],Puz,N_Puz):-
+        puzzle_ref(Puz,H_PosRel,[Cont]),%arranja o elemento onde mudamos
+        e_lista_unitario([Cont]),!,%se for unitario, e nao ve o prox
+        puzzle_muda_propaga(Puz,H_PosRel,[],N_Puz).%executa o muda propaga
+
+puzzle_muda_propaga_aux([H_PosRel|T_PosRel],Puz,N_Puz):-
+        puzzle_ref(Puz,H_PosRel,[Cont]),%arranja o elemento onde mudamos
+        \+ e_lista_unitario([Cont]),%se nao for unitario
+        puzzle_muda_propaga_aux(T_PosRel,Puz,N_Puz).%ve o prox
+
+
+%ve se lista e unitaria
+e_lista_unitario([_|T]):-
+        T == [].
+
+
+
+
--- test.pl
+++ test.pl
@@ -1,3 +1,9 @@
 take(N, _, Xs) :- N =< 0, !, N =:= 0, Xs = [].
 take(_, [], []).
 take(N, [X|Xs], [X|Ys]) :- M is N-1, take(M, Xs, Ys).
+
+test([X|Y]):-writeln(X),writeln(Y).
+test2(X|Y):-writeln(X),writeln(Y).
+
+
+list([_|R]):-R==[].
